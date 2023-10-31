f78e7b56b62927ed9184bb4cd191535c25db8ca5
Ajay Singh <whosinghajay@gmail.com>
2023-10-31 10:03:28 +0530

    verify email

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,6 +2,7 @@ import { connect } from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 connect()
 
@@ -28,6 +29,9 @@ export async function POST(request: NextRequest) {
 
         const savedUser = await newUser.save();
         console.log(savedUser);
+
+        //send verfication email
+        await sendEmail({email, emailType:"VERIFY", userId:savedUser._id});
 
         return NextResponse.json({
             message: "User created Successfully",
